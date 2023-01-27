b92a8a5b5a18d5baaca804fe9288ec9a50d62894
marcelo.teixeira <marcelo.teixeira@softplan.com.br>
2023-01-26 21:58:33 -0300

    Creating boardgame and Position

---
+++ secao-16-projeto-sistema-jogo-de-xadrez/chess-system-java/chess-system/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position pos = new Position(6, 5);
+		
+		System.out.println(pos);
+
+	}
+
+}
