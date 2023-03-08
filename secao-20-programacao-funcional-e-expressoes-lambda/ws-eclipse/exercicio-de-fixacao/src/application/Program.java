e39b43e3697200412125ef26fad7cf3996fb087a
marcelo.teixeira <marcelo.teixeira@softplan.com.br>
2023-03-07 22:34:30 -0300

    Finalizando prog funcional e expressoes lambda

---
+++ secao-20-programacao-funcional-e-expressoes-lambda/ws-eclipse/exercicio-de-fixacao/src/application/Program.java
@@ -0,0 +1,65 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the full file path: ");
+		String path = sc.nextLine();
+		
+		System.out.print("Enter salary: ");
+		double salaryValue = sc.nextDouble();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path)) ){
+			
+			List<Employee> list = new ArrayList<>();
+			
+			String line = br.readLine();
+			while(line != null) {
+				String[] fields = line.split(",");
+				list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+				line = br.readLine();
+			}
+			
+			List<String> emails = list.stream()
+					.filter(e -> e.getSalary() > salaryValue)
+					.map(e -> e.getEmail())
+					.sorted()
+					.collect(Collectors.toList());
+			
+			System.out.println("Email of people whose salary is more than " + String.format("%.2f", salaryValue) + ":");
+			emails.forEach(System.out::println);
+			
+			
+			double sum = list.stream()
+					.filter(e -> e.getName().charAt(0) == 'M')
+					.map(e -> e.getSalary())
+					.reduce(0.0, (x, y) -> x + y);
+			
+			System.out.println("Sum of salary of people whose name starts with 'M': " + String.format("%.2f", sum));
+			
+		} catch (IOException e) {
+			System.out.println("Error " + e.getMessage());
+		} finally {
+			sc.close();
+		}
+		
+
+	}
+
+}
